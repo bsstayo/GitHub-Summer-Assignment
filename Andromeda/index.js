d9725d0585d6573aa1730c50bab46af9e34bd7ae
chanyeong <jcy030896@naver.com>
2023-08-05 17:53:09 +0900

    feat: node entrypoint 파일 추가

--- Andromeda/index.js
+++ Andromeda/index.js
@@ -5,11 +5,11 @@ const app = express()
 app.use(express.static("public"))
 
 app.get('/', (req, res) => {
-    res.send("안녕 웹앱")
+    res.send('hi express')
 })
 
 const server = require("http").createServer({}, app)
 
-server.listen(300, () => {
+server.listen(3000, () => {
     console.log("http를 포트 3000에 오픈합니다")
 })
