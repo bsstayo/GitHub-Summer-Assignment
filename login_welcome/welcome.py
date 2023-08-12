1d8b32da968d7b041420c846e8908ed09ec5e65b
Do-Hoon Kim <k99hoon@gmail.com>
2023-08-12 16:57:06 +0900

    Update welcome.py

--- login_welcome/welcome.py
+++ login_welcome/welcome.py
@@ -1,4 +1,5 @@
 import requests
+import os
 import datetime
 
 def get_random_quote():
@@ -21,12 +22,26 @@ def main():
     quote = get_random_quote()
     joke = get_random_programming_joke()
 
-    print("Hi there! Good Morning\n")
-    print(f"Today is {date}\n")
-    print("Here is a daily quote:\n", quote)
-    print("")
-    print("Have a fun day with this joke ;)\n", joke)
+    path = "./log"
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+    current_date = datetime.datetime.now().strftime('%Y_%m_%d')
+    file_path = os.path.join(path, f"{current_date}_log.txt")
+    file_content = []
+
+    file_content.append("Hi there! Good Morning\n")
+    file_content.append(f"Today is {date}\n")
+    file_content.append("Here is a daily quote:")
+    file_content.append(quote)
+    file_content.append("")
+    file_content.append("Have a fun day with this joke ;)")
+    file_content.append(joke)
+
+    with open(file_path, 'w') as file:
+        for fc in file_content:
+            file.write(fc + '\n')
+            print(fc)
 
 if __name__ == "__main__":
     main()
-
